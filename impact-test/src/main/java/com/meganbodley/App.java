8b0aff5f5035f21a3771fee56c009ee809fe9e77
megan_bodley <23639474@sun.ac.za>
2025-09-15 16:24:13 +0200

    Maven project created and dependency for JUnit5 added

---
+++ impact-test/src/main/java/com/meganbodley/App.java
@@ -0,0 +1,13 @@
+package com.meganbodley;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ impact-test/src/test/java/com/meganbodley/AppTest.java
@@ -0,0 +1,12 @@
+package com.meganbodley;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+
+    @Test
+    void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
